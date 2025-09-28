2cba4b7c8c09f593b196e64408c40c78ca694859
ueno2025 <ken825325@outlook.jp>
2025-09-28 16:36:10 +0900

    Update fetch.js

--- js/fetch.js
+++ js/fetch.js
@@ -2,8 +2,18 @@
 export async function fetch_json(code) {
     const response = await fetch("./data/data.json");
     const companies = await response.json();
+    const input = normalizeText(code);
 
-    const company = companies.find(c => c["証券コード"] == code);
+    const company = companies.find(c => normalizeText(c["証券コード"]) == input) || companies.find(c => normalizeText(c["企業名"]) == input);
+
     return company
     
+}
+
+// 全角・半角、大文字、小文字をそろえる
+function normalizeText(str) {
+    return str
+        .normalize("NFKC")
+        .toUpperCase()
+        .trim();
 }
